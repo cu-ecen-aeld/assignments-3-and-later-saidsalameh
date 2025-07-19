6f67bfc476cba75e78bb4945f70d0a0bc3d47f8c
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2025-07-19 16:28:56 +0200

    Assignment 3.1 after unit test

--- unit-test.sh
+++ unit-test.sh
@@ -4,6 +4,7 @@
 # Automate these steps from the readme:
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
+
 mkdir -p build
 cd build
 cmake ..
